d63753fef963b90430afe1672d52103f42566b94
Roberto Solis Robles <rsolis@uaz.edu.mx>
2020-09-29 17:30:27 -0500

    Inicializacion de archivos de practica

---
+++ PonDatosConDataSource.java
@@ -0,0 +1,140 @@
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+// TODO Sustituya las NNNNNNNN por su nombre completo y las XXXXXXXX por su matricula
+// en las clausulas @author del siguiente comentario Javadoc
+
+/**
+ Esta clase demuestra el uso de DataSource para acceder a la base de datos
+ controlconcursos_ad2020 para colocar datos en tres tablas
+ @author  NNNNNNNN
+ @author  XXXXXXXX
+ */
+public class PonDatosConDataSource {
+    private int idConcurso;
+    private ArrayList<Integer> idSedes;
+
+    // TODO Sustituir las XXX en los valores de los siguientes atributos por su matricula
+    private final String nomSede = "sede_XXX";
+    private final String nomConcurso = "concurso_XXX";
+    private final String nomSedeConcurso = "sede_concurso_XXX";
+    private final String nomtablas[] =
+            { nomSede, nomConcurso, nomSedeConcurso };
+
+    /**
+     * Metodo principal de la clase que recibe la direccion del servidor,
+     * nombre de la base de datos, usuario y clave; datos necesarios para
+     * hacer la conexion con MySQL
+     * @param args Arreglo con los 4 argumentos necesarios para poder ejecutar el programa
+     */
+    public static void main(String args[]){
+        int tipo=15;
+        if (args.length < 4) {
+            System.out.println(
+                    "Uso: java PonDatosConDataSource"
+                            + " dirServidor nombreBaseDatos cuenta password");
+            System.exit(1);
+        }
+        // ESTE IF ES PARA LAS PRUEBAS, IGNORAR
+        if (args.length>=5) {
+            try {
+                tipo=Integer.parseInt(args[4]);
+            }
+            catch (NumberFormatException exnum) { }
+        }
+        PonDatosConDataSource creador = new PonDatosConDataSource();
+        Connection con=null;
+        try{
+            // Cargar el driver de MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = creador.creaConexion(args[0], args[1], args[2], args[3]);
+        }
+        catch(ClassNotFoundException e){
+            System.err.print("No se encontro la clase solicitada: ");
+            System.err.println(e.getMessage());
+        } catch (SQLException esql) {
+            // SI NO SE PUDO CREAR LA CONEXION SE DA LA EXCEPCION SQLException
+            System.err.println("Error al crear conexion a MySQL: "
+                    + esql.getMessage());
+            System.exit(1);
+        }
+        if ((tipo&1)!=0) {
+            if (con instanceof Connection) {
+                System.out.println("Conexion a MySQL exitosa!");
+            }
+        }
+        creador.idSedes = new ArrayList<>();
+        if ((tipo&2)!=0) {
+            creador.llenaTablaSede(con);
+        }
+        else {
+            creador.idSedes.add(1); creador.idSedes.add(2);
+        }
+        if ((tipo&4)!=0) {
+            creador.llenaTablaConcurso(con);
+        }
+        else {
+            creador.idConcurso=1;
+        }
+        if ((tipo&8)!=0) {
+            creador.llenaTablaSedeConcurso(con);
+        }
+
+        // TODO Sustituir NNNNNNNNN por su nombre completo
+        System.out.println("PRACTICA DE NNNNNNNNN");
+    }
+
+    /**
+     * Este metodo crea la conexion a MySQL con los datos recibidos
+     * @param direccionServidor  String que representa direccion del servidor
+     * @param nomBD  String con el nombre de la base de datos a utilizar
+     * @param usuario String con el usuario a usar para conectarse
+     * @param clave  String con la clave a usar para conectarse
+     * @return  Objeto Connection que representa la conexion con MySQL
+     * @throws SQLException  En caso de no poder crearse la conexion
+     */
+    public Connection creaConexion(String direccionServidor,
+                                   String nomBD, String usuario,
+                                   String clave)
+            throws SQLException {
+        // TODO Completar el codigo de creaConexion
+        return null; // TODO: Esta linea se elimina una vez que coloque el codigo real
+    }
+
+    /**
+     * Metodo que coloca datos en la tabla sede
+     * Los datos son tomados del archivo datosSede.txt
+     * NOTA: El codigo demuestra el funcionamiento de un objeto ResultSet
+     * actualizable y como se puede obtener el valor
+     * de un campo autoincrementable despues de agregar el registro
+     * @param con Objeto Connection que representa la conexion a MySQL
+     */
+    public void llenaTablaSede(Connection con) {
+        // TODO Completar el codigo del metodo llenaTablaSede
+
+    } // fin del metodo llenaTablaSede()
+
+    /**
+     * Metodo que coloca datos en la tabla concurso
+     * NOTA: El codigo demuestra como se puede obtener el valor
+     * de un campo autoincrementable despues de agregar el registro
+     * @param con Objeto Connection que representa la conexion a MySQL
+     */    
+    public void llenaTablaConcurso(Connection con) {
+        // TODO Completar el codigo del metodo llenaTablaConcurso
+
+    }  // fin del metodo llenaTablaConcurso()
+
+    /**
+     * Metodo que coloca datos en la tabla sede_concurso
+     * @param con Objeto Connection que representa la conexion a MySQL
+     */
+    public void llenaTablaSedeConcurso(Connection con) {
+        // TODO Completar el codigo del metodo llenaTablaSedeConcurso
+
+    }  // fin del método llenaTablaSedeConcurso()
+
+
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# PRÁCTICA 6. USO DE LA CLASE DriverManager DE JDBC PARA ACCEDER A MySQL.
+
+## COPIA DEL REPOSITORIO REMOTO EN SU COMPUTADORA LOCAL
+Como primer paso, será necesario crear una copia local del repositorio remoto creado en Github al aceptar la tarea. Para ello, es necesario hacer los siguientes pasos:
+1)	Entrar a la página cuyo URL les fue proporcionado al aceptar la tarea, en tal página dé click en el botón Code y copie el URL que aparece en el cuadro de texto de nombre **Clone with HTTPS** (comienza con *https://*)
+2)	En una consola de Git Bash en Windows (o en una terminal en Linux o Mac), cree una carpeta donde quiera que se contengan sus prácticas del semestre (si es que aún no la has creado) y colócate en tal carpeta. La carpeta la puedes crear desde el Git Bash o terminal Linux/Mac usando el comando `mkdir` (o con el explorador de archivos de su sistema operativo) y en la consola de Git Bash o terminal de Linux/Mac te puedes cambiar a la carpeta mencionada usando el comando `cd`
+3)	Clone el repositorio privado dando el comando `git clone URL practica07`
+ (donde **URL** es el URL que copió en el paso 1)\
+ Este comando creará dentro de la carpeta creada en el paso 2) una subcarpeta de nombre **practica07** donde estará una copia local del repositorio remoto.\
+ Los comandos posteriores de git tendrán que darse desde tal carpeta, por tanto, cámbiate a la carpeta usando `cd practica07`
+
+
+## REQUERIMIENTOS
+
+La lista de requerimientos que deben cumplirse en esta práctica los puede encontrar en el archivo **PracticasLaboratorio07_30Sep2020.pdf**
+
+
+## CALIFICACIÓN
+
+La calificación para esta práctica no les será emitida de manera inmediata al subir su solución, sin embargo se realizarán pruebas para verificar que compila y se ejecuta su programa sin problemas. Asegúrate que pase todas las pruebas antes de considerar tu práctica como terminada.
+
+## NOTAS IMPORTANTES
+
+1. Recuerda que el proceso que debes estar haciendo es:
+   - Haz cambios  en el archivo fuente **PonDatosConDataSource.java** 
+   - **Compila el programa en tu computadora para ver que los cambios que introdujiste no causan ningún error** (trata de ir haciendo cambios incrementales, por ejemplo, completa uno de los métodos primero y prueba que compile antes de hacer cambios en los otros métodos)
+   - Si ya compila avisa a tu repositorio local que registre esos cambios dando los comandos `git add PonDatosConDataSource.java` y `git commit -m "MENSAJE"` donde MENSAJE es un texto que describe brevemente, pero de manera clara los cambios que realizaste desde el último commit. 
+
+2. Una vez completados todos los métodos, ejecúta el programa de acuerdo a las instrucciones y verifica que no genera errores y muestra los mensajes esperados. 
+
+3. Si ya se ejecuta correctamente, súbelo al repositorio remoto dando `git push`. De otra manera, corrige los errores (haciendo las compilaciones, git add y git commit correspondientes)
+
+4. Cada vez que hagas `git push` se realizarán automáticamente pruebas sobre tu código para verificar si funciona correctamente. Para esta práctica en particular no se te proporcionará una calificación de manera inmediata pues debe ser revisada con mayor detalle. Recuerda que en la página de tu repositorio en la sección **Pull Requests**, se encuentra una subsección de nombre **Feedback**, donde podrás encontrar los resultados de las pruebas en la pestaña denominada **Check** (expandiendo la parte que dice **Run education/autograding@v1**), y cualquier comentario general que el profesor tenga sobre tu código en la pestaña **Conversation**. 
